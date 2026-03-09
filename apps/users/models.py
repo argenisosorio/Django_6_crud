67e948eb82b71bb40a101d42559165ad6e281ffa
Argenis Osorio <argenisosorio580@hotmail.com>
2026-03-09 15:43:07 -0400

    feat(Models): Ajustado el modelo para pruebas de importación de data csv desde Laravel

--- apps/users/models.py
+++ apps/users/models.py
@@ -20,12 +20,26 @@ class User(AbstractUser):
     añade un campo role opcional para capturar el rol del usuario dentro de la
     aplicación.
     """
+    # id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255, default="")
+    # email = models.CharField(unique=True, max_length=255)
+    email_verified_at = models.DateTimeField(blank=True, null=True)
+    # password = models.CharField(max_length=255)
+    role = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        db_comment="Tipo de role:\n ADM = Administrador,\n SUP = Supervisor,\n OPD = Operador de donantes,\n OPS = Operador de solicitudes,\n PIN = Personal de inmunología,\n CHO = Coordinador hospitalario\n USR = Usuario,",
+    )
+    remember_token = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(
+        blank=True,
+        null=True,
+        db_comment="Fecha y hora en la que el registro fue eliminado",
+    )
 
-    role = models.CharField(max_length=3, blank=True, null=True)
-
-    def __str__(self) -> str:
-        """
-        Devuelve el nombre de usuario como una representación de cadena.
-        """
-
-        return self.username
+    class Meta:
+        # managed = False
+        db_table = "users"
