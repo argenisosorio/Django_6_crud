99c98fb3d57434dbf03b521a6f87a36566477674
Argenis Osorio <argenisosorio580@hotmail.com>
2026-03-16 16:18:01 -0400

    test(Validations): Agregado ejemplo básico de validación desde el modelo de Django.

--- apps/person/models.py
+++ apps/person/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 class Person(models.Model):
     """
@@ -9,7 +10,16 @@ class Person(models.Model):
     """
 
     # Personal Information Fields
-    name = models.CharField(max_length=100)
+    name = models.CharField(
+        max_length=5,
+        validators=[RegexValidator(r'\d', inverse_match=True, message='No se permiten números.')],
+        error_messages={
+            'max_length': 'Máximo 5 caracteres.',
+            'blank': 'Este campo es requerido.',
+            'required': 'Este campo es requerido.',
+        }
+    )
+
     email = models.EmailField()
     age = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
