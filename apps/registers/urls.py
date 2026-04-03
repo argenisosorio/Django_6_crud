0e805fe65b0ba486af76b2e88814339784cf966d
Argenis Osorio <argenisosorio580@hotmail.com>
2026-04-03 13:24:24 -0400

    feat: Agregados nuevos campos al formulario de la quiniela.

--- apps/registers/urls.py
+++ apps/registers/urls.py
@@ -8,7 +8,7 @@ app_name = 'registers'
 urlpatterns = [
     path('', views.home, name='home'),
     path('create_update_register/', views.create_update_register, name='create_update_register'),
-    path('<int:pk>/', views.detail_register, name='detail'),
-    path('<int:pk>/update/', views.update_register, name='update'),
-    path('<int:pk>/delete/', views.delete_register, name='delete'),
+    #path('<int:pk>/', views.detail_register, name='detail'),
+    #path('<int:pk>/update/', views.update_register, name='update'),
+    #path('<int:pk>/delete/', views.delete_register, name='delete'),
 ]
